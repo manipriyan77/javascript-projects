c59579930c5e9a63a167881e743a43bdb28c562f
manipriyan77 <manipriyangopalan@gmail.com>
2023-08-15 09:15:19 +0530

    completed learning sets and maps

--- maps.js
+++ maps.js
@@ -1,25 +1,76 @@
-// method 1 without maps
-const CURRENCY_MAP = {
-  "United States": "USD",
-  India: "Rupee",
-};
+// // method 1 without maps
+// const CURRENCY_MAP = {
+//   "United States": "USD",
+//   India: "Rupee",
+// };
 
-const currency = CURRENCY_MAP["India"];
+// const currency = CURRENCY_MAP["India"];
 
-// using map
-//using map the key can be anything whereas
-//in object the keys can only be a string
-const map = new Map([
-  [{ a: 1 }, "USD"],
-  ["India", "Rupee"],
-]);
-console.log(map);
+// // using map
+// //using map the key can be anything whereas
+// //in object the keys can only be a string
+// const map = new Map([
+//   [{ a: 1 }, "USD"],
+//   ["India", "Rupee"],
+// ]);
+// console.log(map);
 
-const user = {
+// const user = {
+//   name: "Manipriyan",
+// };
+
+// const userMap = new Map([[user, { age: 25 }]]);
+// console.log(userMap);
+// // to get a value from map
+// console.log(userMap.get(user));
+
+const dummyObj = {
   name: "Manipriyan",
+  age: null,
+  address: {
+    street: "adas",
+    state: "Tn",
+    pin: null,
+    permanentAddress: {
+      street: "adas",
+      state: "Tn",
+      pin: null,
+    },
+  },
 };
 
-const userMap = new Map([[user, { age: 25 }]]);
-console.log(userMap);
-// to get a value from map
-console.log(userMap.get(user));
+function findNullKeys(obj, prefix = "") {
+  const result = [];
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (value === null) {
+      result.push(key);
+    } else if (typeof value === "object" && value !== null) {
+      result.push(...findNullKeys(value, prefix + key + "."));
+    }
+  }
+
+  return result;
+}
+
+const nullKeys = findNullKeys(dummyObj);
+console.log(nullKeys);
+
+const question = new Map([
+  ["question", "Best programming language??"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "Javascript"],
+  ["correct", 3],
+  [true, "correct"],
+  [flase, "Try again"],
+]);
+
+console.log(question);
+
+// Convert an object to an Map
+
+// const hoursMao = new Map(Object.entries(openingHours));
+
+// Convert map to an array
+const newArrayValue = [...question];
--- sets.js
+++ sets.js
@@ -1,21 +1,40 @@
-const set = new Set([1, 2, 3, 4, 4, 4]);
-console.log(set);
-set.add(6);
+// const set = new Set([1, 2, 3, 4, 4, 4]);
+// console.log(set);
+// set.add(6);
 
-// we can't able to get inidividual values like set[0]
-console.log(set);
-// to check if the set has some value
-console.log(set.has(6)); // true
-// To delete a value
-set.delete(3); // {1,2,4,6}
-console.log(set.size());
-set.clear();
+// // we can't able to get inidividual values like set[0]
+// console.log(set);
+// // to check if the set has some value
+// console.log(set.has(6)); // true
+// // To delete a value
+// set.delete(3); // {1,2,4,6}
+// console.log(set.size());
+// set.clear();
 
-// remove duplicates
+// // remove duplicates
 
-function removeDuplicates(arr) {
-  const set = new Set(arr);
-  return [...set];
+// function removeDuplicates(arr) {
+//   const set = new Set(arr);
+//   return [...set];
+// }
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 5, 6, 7]));
+
+// Set is a collection of unique values
+const orderSet = new Set(["Pizza", "Pizza", "Rice", "Dal", "Milk"]);
+console.log(orderSet);
+console.log(new Set("manipriyan"));
+
+console.log(orderSet.size);
+// to check certain element
+console.log(orderSet.has("Pizza"));
+// to add
+orderSet.add("Bread");
+orderSet.add("Bread");
+console.log(orderSet);
+// to delete
+orderSet.delete("Bread");
+
+for (const order of orderSet) {
+  console.log(order);
 }
-
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 5, 6, 7]));
