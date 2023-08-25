9c1f3059a8ef179e0bdcdca319bc522cca3d78c5
manipriyan77 <manipriyangopalan@gmail.com>
2023-08-25 21:37:18 +0530

    started working on e commerce project

--- stringMethods.js
+++
@@ -1,9 +0,0 @@
-const airline = "TAP Air Porrtugal";
-const plane = "A320";
-
-console.log(plane[0]);
-console.log(airline.length);
-console.log("Mani".length);
-
-console.log(airline.indexOf("r"));
-console.log(airline.lastIndexOf("r"));
